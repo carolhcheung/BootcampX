e3fd49686676a69bd4f30b745c89794e24653f6a
Carol Cheung <carol.cheung@telus.com>
2022-12-14 13:46:57 -0800

    adding parameterized query to avoid sql injection

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -8,16 +8,37 @@ const pool = new Pool({
 });
 
 //makes node arguments into array of arguments [cohort, limit] 
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+const values = [`%${cohortName}%`, limit];  
+//prevents malicious code injection
+//by setting the only values that will be taken from client 
 
-pool.query(`
+const queryString = `
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort_name
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`)
-.then(res => {
-  res.rows.forEach(user => {
-    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
-  })
-}).catch(err => console.error('query error', err.stack));
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`;
+
+pool.query(queryString, values)
+  .then(res => { //res.rows is the objects from the res which is array of objects
+    res.rows.forEach(user => { //user = name argument anything you want it will reference to the array of objects from res.rows and forEach(ojb[0], obj[1]...do something)
+      console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort_name} cohort`);
+    })
+  }).catch(err => console.error('query error', err.stack));
+
+
+// pool.query(`
+// SELECT students.id as student_id, students.name as name, cohorts.name as cohort_name
+// FROM students
+// JOIN cohorts ON cohorts.id = cohort_id
+// WHERE cohorts.name LIKE '%${process.argv[2]}%'
+// LIMIT ${process.argv[3] || 5};
+// `)
+//   .then(res => { //res.rows is the objects from the res which is array of objects
+//     res.rows.forEach(user => { //user = name argument anything you want it will reference to the array of objects from res.rows and forEach(ojb[0], obj[1]...do something)
+//       console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort_name} cohort`);
+//     })
+//   }).catch(err => console.error('query error', err.stack));
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,18 +7,40 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-
-pool.query(`
+const cohortName = process.argv[2] || 'JUL02';
+const values = [`${cohortName}`];
+const queryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name LIKE $1
 ORDER BY teacher;
-`)
-  .then(res => {
-    res.rows.forEach(row => {
+`;
+
+pool.query(queryString, values)
+  .then(res => {  //res.rows is the objects from the res which is array of objects
+    res.rows.forEach(row => {  //row = name argument anything you want it will reference to the array of objects from res.rows and forEach(ojb[0], obj[1]...do something)
       console.log(`${row.cohort}: ${row.teacher}`);
+      // console.log(process.argv[2])
+      // console.log(res.rows);
     })
   }).catch(err => console.error('query error', err.stack));
+
+// pool.query(`
+// SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+// FROM teachers
+// JOIN assistance_requests ON teacher_id = teachers.id
+// JOIN students ON student_id = students.id
+// JOIN cohorts ON cohort_id = cohorts.id
+// WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+// ORDER BY teacher;
+// `)
+//   .then(res => {  //res.rows is the objects from the res which is array of objects
+//     res.rows.forEach(row => {  //row = name argument anything you want it will reference to the array of objects from res.rows and forEach(ojb[0], obj[1]...do something)
+//       // console.log(process.argv[2])
+//       // console.log(res.rows);
+//       console.log(`${row.cohort}: ${row.teacher}`);
+//     })
+//   }).catch(err => console.error('query error', err.stack));
